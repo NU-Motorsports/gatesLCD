2ae4462336fb46ff3e8fe1060f115ef12aeb002b
wcreuss <57601477+wcreuss@users.noreply.github.com>
2019-11-19 18:27:44 -0500

    Add files via upload

--- gates.ino
+++ gates.ino
@@ -1,10 +1,6 @@
 //NOTE: REQUIRES ADAFRUIT LIBRARY FOR RGBLCDSHEILD
 //Link: https://learn.adafruit.com/rgb-lcd-shield/using-the-rgb-lcd-shield
 
-int start_pin = 2;
-int finish_pin = 3;
-unsigned long start_time = 0;
-float run_time;
 
 // include the library code:
 #include <Wire.h>
@@ -14,13 +10,13 @@ float run_time;
 #include <Adafruit_MCP23017.h>
 #include <Adafruit_RGBLCDShield.h>
 
-//#include <EEPROM.h>
-
 //declare lcd object
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
-int addr = 0;
-int tNum = 1;
+int start_pin = 2;
+int finish_pin = 3;
+unsigned long start_time = 0;
+float run_time = 0;
 
 void setup()
 {
@@ -43,13 +39,10 @@ void start()
 {
     if (start_time == 0)
     {
-        //start timer
         start_time = millis();
         
         //notify start
         Serial.println("start triggered");
-        lcd.setCursor(0, 0);
-        lcd.print("start triggered");
     }
 }
 
@@ -64,18 +57,24 @@ void finish()
         run_time = (float)(start_time) / 1000;
         //output result
         Serial.println(run_time,3);
-        lcd.setCursor(0, 1);
-        lcd.print(run_time);
-//        EEPROM.write(addr, run_time);
         //reset start time
         start_time = 0;
-        addr = addr + 1;
-//        if (addr == EEPROM.length()) {
-//          addr = 0;
-//        }
     }
 }
 
 void loop()
 {
+  lcd.setCursor(0, 0);
+  lcd.print(run_time);
+
+  lcd.setCursor(0, 1);
+  if (start_time > 0)
+  {
+    lcd.print((float)(millis() - start_time) / 1000);
+    lcd.print("     ");
+  }  
+  else
+  {
+    lcd.print("READY");
+  }
 }
